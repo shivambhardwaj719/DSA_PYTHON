6e0c42d9d0d1bf8531cd068ccf332b48d5f62975
shivambhardwaj719 <115762160+shivambhardwaj719@users.noreply.github.com>
2024-03-19 21:25:16 +0530

    added merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,33 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+
+        left_arr = arr[:len(arr) // 2]
+        right_arr = arr[len(arr) // 2:]
+
+        mergeSort(left_arr)
+        mergeSort(right_arr)
+
+        i = j = k = 0
+
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] <= right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+arr = [5, 6, 3, 2, 1, 8, 7, 9]
+mergeSort(arr)
+print(arr)
